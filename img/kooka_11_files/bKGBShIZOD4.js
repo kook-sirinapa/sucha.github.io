b5614622019904703d0a3b11e208edb7581da4c3
Sirinapa Suraso <sirinapa.cto@gmail.com>
2021-06-06 10:59:29 +0700

    Innitial commit

---
+++ img/kooka_11_files/bKGBShIZOD4.js
@@ -0,0 +1,4 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["4YcRD"]); }
+
+__d("VideoHomeTypedLoggerLite",["Banzai","generateLiteTypedLogger"],(function(a,b,c,d,e,f){"use strict";e.exports=b("generateLiteTypedLogger")("logger:VideoHomeLoggerConfig")}),null);
+__d("XVideoHomeFeedController",["XController"],(function(a,b,c,d,e,f){e.exports=b("XController").create("/watch/",{comment_id:{type:"FBID"},cursor:{type:"String"},ref:{type:"String"},t:{type:"Int"},v:{type:"String"},external_log_id:{type:"String"},q:{type:"String"}})}),null);
